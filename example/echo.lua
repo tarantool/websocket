5f1864e417315d5dfebd82cecbef97199af1378a
Michael Filonenko <filonenko.mikhail@gmail.com>
2018-08-05 09:28:03 +0300

    reworked examples

--- example/echo.lua
+++ example/echo.lua
@@ -1,45 +1,34 @@
 #!/usr/bin/env tarantool
 
 local log = require('log')
-local fiber = require('fiber')
-local wsserver = require('websocket')
+local socket = require('socket')
+local websocket = require('websocket')
 local json = require('json')
---------------------
-wsd = wsserver.new('127.0.0.1', 8080)
 
-wsd:listen()
+socket.tcp_server(
+    '0.0.0.0',
+    8080,
+    function(sock)
+        local wspeer = websocket.new(sock)
 
-fiber.create(
-    function(wsd)
-        while wsd:is_listen() do
-            local channel = wsd:accept()
-
-            if channel ~= nil then
-                fiber.create(
-                    function (channel)
-                        while true do
-                            local message = channel:read()
-                            if message == nil then
-                                log.info('Closed while read')
-                                break
-                            end
-                            log.info('echo '..json.encode(message))
-                            local rc, err = channel:write(message)
-                            if rc == nil then
-                                log.info('Closed while write %s', err)
-                                break
-                            end
-                        end
-                    end, channel)
+        while true do
+            local message, err = wspeer:read()
+            if message then
+                if message.opcode == nil then
+                    log.info('Normal close')
+                    break
+                end
+                log.info('echo ' .. json.encode(message))
+                wspeer:write(message)
             else
-                log.info('Websocket server is closed')
+                log.info('Exception close ' .. tostring(err))
+                if wspeer:error() then
+                    log.info('Socket error '..wspeer:error())
+                end
                 break
             end
         end
-    end, wsd)
-
--- if wsserver:is_listen() then
---   wsd:close()
+end)
 
 local console = require('console')
 console.start()
